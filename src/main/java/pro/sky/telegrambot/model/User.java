032389243bde692925b908dbbd273dc0f4d7db67
EvgenyEVS <evis10@yandex.ru>
2026-03-28 11:04:06 +0300

    4

--- src/main/java/pro/sky/telegrambot/model/User.java
+++ src/main/java/pro/sky/telegrambot/model/User.java
@@ -1,4 +1,65 @@
 package pro.sky.telegrambot.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "chatId")
+@ToString
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long chatId;
+
+    private String telegramUserName;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String phoneNumber;
+
+    //@OneToMany()
+    private Set<Animal> pets = new HashSet<>();
+
+    //@OneToMany()
+    private List<Report> userReports = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private ShelterType shelterType = ShelterType.SHELTER_UNKNOWN;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus = UserStatus.NON_ACTIVE;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "assigned_volunteer_id")
+//    private Volunteer assignedVolunteer;
+
+
+
+    public enum ShelterType {
+        SHELTER_UNKNOWN,
+        SHELTER_CAT,
+        SHELTER_DOG
+    }
+    public enum UserStatus {
+        ORDINARY,
+        ADOPTER,
+        GRADUATED,
+        NON_ACTIVE
+    }
+
+
+
+
 }
