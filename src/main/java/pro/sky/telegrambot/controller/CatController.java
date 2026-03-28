b561c67226aef96c7f1be82b803fe8bf1edd98ac
EvgenyEVS <evis10@yandex.ru>
2026-03-28 16:08:41 +0300

    7

---
+++ src/main/java/pro/sky/telegrambot/controller/CatController.java
@@ -0,0 +1,15 @@
+package pro.sky.telegrambot.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.telegrambot.model.Cat;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/cats")
+public class CatController {
+
+}
--- src/main/java/pro/sky/telegrambot/model/Cat.java
+++ src/main/java/pro/sky/telegrambot/model/Cat.java
@@ -1,4 +1,20 @@
 package pro.sky.telegrambot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+
+
 public class Cat extends Pet {
+
+    public Cat() {
+        super();
+    }
 }
