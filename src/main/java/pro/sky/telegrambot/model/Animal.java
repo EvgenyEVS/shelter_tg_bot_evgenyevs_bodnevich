bc0dc7f478ebd84109e5fc49c78b42fcf76af53c
EvgenyEVS <evis10@yandex.ru>
2026-03-28 11:16:49 +0300

    5

--- src/main/java/pro/sky/telegrambot/model/Animal.java
+++ src/main/java/pro/sky/telegrambot/model/Animal.java
@@ -1,4 +1,38 @@
 package pro.sky.telegrambot.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@ToString
+
 public abstract class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+    protected String name;
+    protected Integer age;
+    protected Gender gender = Gender.UNKNOWN;
+    protected boolean castratedOrSpayed;
+
+    @ManyToOne
+    protected User owner;
+
+
+
+    protected enum Gender {
+        MALE,
+        FEMALE,
+        UNKNOWN
+    }
+
+
 }
