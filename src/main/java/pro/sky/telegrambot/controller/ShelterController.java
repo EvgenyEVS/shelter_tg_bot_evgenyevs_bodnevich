974e31e6557363844e923bbd3c673b6af27b2dad
EvgenyEVS <evis10@yandex.ru>
2026-04-13 22:22:16 +0300

    17

--- src/main/java/pro/sky/telegrambot/controller/ShelterController.java
+++ src/main/java/pro/sky/telegrambot/controller/ShelterController.java
@@ -2,9 +2,7 @@ package pro.sky.telegrambot.controller;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pro.sky.telegrambot.dto.shelterDto.ShelterContactsDto;
 import pro.sky.telegrambot.dto.shelterDto.ShelterGeneralInfoDto;
 import pro.sky.telegrambot.model.Shelter;
@@ -13,7 +11,7 @@ import pro.sky.telegrambot.service.ShelterService;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("/shelter")
 public class ShelterController {
 
@@ -32,17 +30,17 @@ public class ShelterController {
     }
 
     @GetMapping()
-    public Shelter getShelter(PetType petType) {
+    public Shelter getShelter(@RequestParam PetType petType) {
         return shelterService.getShelterByPetType(petType);
     }
 
     @GetMapping("/general_info")
-    public ShelterGeneralInfoDto getGeneralInfo(PetType petType) {
+    public ShelterGeneralInfoDto getGeneralInfo(@RequestParam PetType petType) {
         return shelterService.getGeneralInfo(petType);
     }
 
     @GetMapping("/contacts")
-    public ShelterContactsDto get_contacts(PetType petType) {
+    public ShelterContactsDto get_contacts(@RequestParam PetType petType) {
         return shelterService.getContacts(petType);
     }
 
